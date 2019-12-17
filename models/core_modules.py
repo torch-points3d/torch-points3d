a220ba5cb01cb1c29de556c79d8b9592214bff78
Thomas Chaton <thomas.chaton.ai@gmail.com>
2019-12-17 17:23:53 +0000

    'BaseConvolution updating'

--- models/core_modules.py
+++ models/core_modules.py
@@ -6,10 +6,12 @@ from torch.nn import Sequential as Seq, Linear as Lin, ReLU, LeakyReLU, BatchNor
 from torch_geometric.nn import knn_interpolate, fps, radius, global_max_pool, global_mean_pool, knn
 from torch_geometric.data import Batch
 
+
 def copy_from_to(data, batch):
     for key in data.keys:
         if key not in batch.keys:
             setattr(batch, key, getattr(data, key, None))
+
 
 def MLP(channels, activation=ReLU()):
     return Seq(*[
@@ -17,48 +19,27 @@ def MLP(channels, activation=ReLU()):
         for i in range(1, len(channels))
     ])
 
+################## BASE CONVOLUTION #####################
 
-class FPModule(torch.nn.Module):
-    """ Upsampling module from PointNet++
-
-    Arguments:
-        k [int] -- number of nearest neighboors used for the interpolation
-        up_conv_nn [List[int]] -- list of feature sizes for the uplconv mlp
-
-    Returns:
-        [type] -- [description]
-    """
-
-    def __init__(self, up_k, up_conv_nn, *args, **kwargs):
-        super(FPModule, self).__init__()
-        self.k = up_k
-        self.nn = MLP(up_conv_nn)
-
-    def forward(self, data):
-        batch_obj = Batch()
-        data, data_skip = data
-        x, pos, batch, x_skip, pos_skip, batch_skip = data.x, data.pos, data.batch, data_skip.x, data_skip.pos, data_skip.batch
-        x = knn_interpolate(x, pos, pos_skip, batch, batch_skip, k=self.k)
-        if x_skip is not None:
-            x = torch.cat([x, x_skip], dim=1)
-        batch_obj.x = self.nn(x)
-        copy_from_to(data_skip, batch_obj)
-        return batch_obj
 
 class BaseConvolution(ABC, torch.nn.Module):
+
     def __init__(self, sampler, neighbour_finder, *args, **kwargs):
         torch.nn.Module.__init__(self)
 
         self.sampler = sampler
         self.neighbour_finder = neighbour_finder
 
+
+class BaseConvolutionDown(BaseConvolution):
+    def __init__(self, sampler, neighbour_finder, *args, **kwargs):
+        super(BaseConvolutionDown, self).__init__(sampler, neighbour_finder, *args, **kwargs)
+
         self._precompute_multi_scale = kwargs.get("precompute_multi_scale", None)
         self._index = kwargs.get("index", None)
 
-    @property
-    @abstractmethod
-    def conv(self):
-        pass
+    def conv(self, x, pos, edge_index):
+        raise self._conv(x, pos, edge_index)
 
     def forward(self, data):
         batch_obj = Batch()
@@ -76,23 +57,79 @@ class BaseConvolution(ABC, torch.nn.Module):
         copy_from_to(data, batch_obj)
         return batch_obj
 
-class BaseConvolutionUP(ABC, torch.nn.Module):
-    def __init__(self, sampler, neighbour_finder, *args, **kwargs):
-        torch.nn.Module.__init__(self)
 
-        self.sampler = sampler
-        self.neighbour_finder = neighbour_finder
+class BaseConvolutionUp(BaseConvolution):
+    def __init__(self, neighbour_finder, *args, **kwargs):
+        super(BaseConvolutionUp, self).__init__(None, neighbour_finder, *args, **kwargs)
 
         self._precompute_multi_scale = kwargs.get("precompute_multi_scale", None)
         self._index = kwargs.get("index", None)
+        self._skip = kwargs.get("skip", True)
 
-    @property
-    @abstractmethod
-    def conv(self):
-        pass
+    def conv(self, x, pos, pos_skip, batch, batch_skip, edge_index):
+        raise NotImplementedError
 
     def forward(self, data):
-        raise NotImplementedError
+        batch_obj = Batch()
+        data, data_skip = data
+        x, pos, batch, x_skip, pos_skip, batch_skip = data.x, data.pos, data.batch, data_skip.x, data_skip.pos, data_skip.batch
+
+        if self.neighbour_finder is not None:
+            if self._precompute_multi_scale:
+                edge_index = getattr(data_skip, "edge_index_{}".format(self._index), None)
+                edge_index = torch.cat([edge_index[-1], edge_index[0]], -1)  # Reverse the graph
+            else:
+                row, col = self.neighbour_finder(pos, pos_skip, batch, batch_skip)
+                edge_index = torch.stack([col, row], dim=0)
+        else:
+            edge_index = None
+
+        x = self.conv(x, pos, pos_skip, batch, batch_skip, edge_index)
+
+        if x_skip is not None and self._skip:
+            x = torch.cat([x, x_skip], dim=1)
+        batch_obj.x = self.nn(x)
+        copy_from_to(data_skip, batch_obj)
+        return batch_obj
+
+
+class GlobalBaseModule(torch.nn.Module):
+    def __init__(self, nn, aggr='max'):
+        super(GlobalBaseModule, self).__init__()
+        self.nn = MLP(nn)
+        self.pool = global_max_pool if aggr == "max" else global_mean_pool
+
+    def forward(self, data):
+        batch_obj = Batch()
+        x, pos, batch = data.x, data.pos, data.batch
+        x = self.nn(torch.cat([x, pos], dim=1))
+        batch_obj.x = self.pool(x, batch)
+        batch_obj.pos = pos.new_zeros((x.size(0), 3))
+        batch_obj.batch = torch.arange(x.size(0), device=batch.device)
+        copy_from_to(data, batch_obj)
+        return batch_obj
+
+#################### COMMON MODULE ########################
+
+
+class FPModule(BaseConvolutionUp):
+    """ Upsampling module from PointNet++
+
+    Arguments:
+        k [int] -- number of nearest neighboors used for the interpolation
+        up_conv_nn [List[int]] -- list of feature sizes for the uplconv mlp
+
+    Returns:
+        [type] -- [description]
+    """
+
+    def __init__(self, up_k, up_conv_nn, *args, **kwargs):
+        super(FPModule, self).__init__(None)
+        self.k = up_k
+        self.nn = MLP(up_conv_nn)
+
+    def conv(self, x, pos, pos_skip, batch, batch_skip, *args):
+        return knn_interpolate(x, pos, pos_skip, batch, batch_skip, k=self.k)
 
 
 class BaseResnetBlock(ABC, torch.nn.Module):
--- models/pointnet2/modules.py
+++ models/pointnet2/modules.py
@@ -2,7 +2,8 @@ from torch_geometric.nn import PointConv
 from models.core_modules import *
 from models.core_sampling_and_search import RadiusNeighbourFinder, FPSSampler
 
-class SAModule(BaseConvolution):
+
+class SAModule(BaseConvolutionDown):
     def __init__(self, ratio=None, radius=None, down_conv_nn=None, *args, **kwargs):
         super(SAModule, self).__init__(FPSSampler(ratio), RadiusNeighbourFinder(radius), *args, **kwargs)
 
