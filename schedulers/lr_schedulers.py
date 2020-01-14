39be6c8f69cf152a9cc9f5df4ba83b3c142ed5f1
Nicolas Chaulet <nicolas.chaulet@gmail.com>
2020-01-14 09:19:18 +0000

    Adding scheduler

---
+++ schedulers/lr_schedulers.py
@@ -0,0 +1,28 @@
+from torch.optim.lr_scheduler import LambdaLR
+
+
+def get_scheduler(learning_rate_params, optimizer):
+    """ Builds and sets a learning rate scheduler on a given optimizer
+
+    Arguments:
+        learning_rate_params -- all parameters required for setting the learning rate
+        optimizer -- Optimizer affected by the scheduler
+
+    Returns:
+        LRScheduler
+    """
+    scheduler_builder = _LR_REGISTER.get(learning_rate_params.scheduler_type, None)
+    if scheduler_builder is None:
+        return None
+    return scheduler_builder(learning_rate_params, optimizer)
+
+
+def _build_step_decay(learning_rate_params, optimizer):
+    lr_lbmd = lambda e: max(
+        learning_rate_params.lr_decay ** (e // learning_rate_params.decay_step),
+        learning_rate_params.lr_clip / learning_rate_params.base_lr,
+    )
+    return LambdaLR(optimizer, lr_lbmd)
+
+
+_LR_REGISTER = {"step_decay": _build_step_decay}
