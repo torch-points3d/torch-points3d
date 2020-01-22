38240bd218ba8eaaebd1599b3caba0b2c1ef6c37
humanpose1 <sofiane.horache@mines-paristech.fr>
2020-01-22 17:27:13 +0100

    compute fragment from rgbd frames

--- src/datasets/registration/reg3dmatch_dataset.py
+++ src/datasets/registration/reg3dmatch_dataset.py
@@ -1,17 +1,46 @@
+import collections
+import errno
 import logging
 import os
 import os.path as osp
 import shutil
 import torch
-from torch_geometric.data import InMemoryDataset, Data, download_url, extract_zip
+from torch_geometric.data import Dataset, Data, download_url, extract_zip
 from torch_geometric.data import DataLoader
 
-from .base_dataset import BaseDataset
+from src.datasets.base_dataset import BaseDataset
+from src.datasets.registration.utils import rgbd2fragment
 
 log = logging.getLogger(__name__)
 
-def create_fragment():
-    pass
+
+def to_list(x):
+    """
+    taken from https://pytorch-geometric.readthedocs.io/en/latest/_modules/torch_geometric/data/dataset.html#Dataset
+    """
+    if not isinstance(x, collections.Iterable) or isinstance(x, str):
+        x = [x]
+    return x
+
+
+def files_exist(files):
+    """
+    taken from https://pytorch-geometric.readthedocs.io/en/latest/_modules/torch_geometric/data/dataset.html#Dataset
+    """
+
+    return all([osp.exists(f) for f in files])
+
+
+def makedirs(path):
+    """
+    taken from https://github.com/rusty1s/pytorch_geometric/blob/master/torch_geometric/data/makedirs.py
+    """
+    try:
+        os.makedirs(osp.expanduser(osp.normpath(path)))
+    except OSError as e:
+        if e.errno != errno.EEXIST and osp.isdir(path):
+            raise e
+
 
 def get_urls(filename):
     res = []
@@ -20,28 +49,18 @@ def get_urls(filename):
     return res
 
 
-class Descr3DMatch(InMemoryDataset):
+class General3DMatch(Dataset):
 
-    list_urls_sun3d = get_urls(
-        osp.join('urls', 'url_sun3d.txt'))
-    list_urls_7_scenes = get_urls(
-        osp.join('urls', 'url_7-scenes.txt'))
-    list_urls_analysis_by_synthesis = get_urls(
-        osp.join('urls', 'url_analysis-by-synthesis.txt'))
-    list_urls_bundlefusion = get_urls(
-        osp.join('urls', 'url_bundlefusion.txt'))
-    list_urls_rgbd_scene_v2 = get_urls(
-        osp.join('urls', 'url_rgbd-scenes-v2.txt'))
-    dict_urls = dict(sun3d=list_urls_sun3d,
-                     seven_scenes=list_urls_7_scenes,
-                     analysis_by_synthesis=list_urls_analysis_by_synthesis,
-                     bundlefusion=list_urls_bundlefusion,
-                     rgbd_scene_v2=list_urls_rgbd_scene_v2)
+    list_urls_train = get_urls(osp.join('urls', 'url_train.txt'))
+    list_urls_train_small = get_urls(osp.join('urls', 'url_train_small.txt'))
+    list_urls_test = get_urls(osp.join('urls', 'url_test.txt'))
+    dict_urls = dict(train=list_urls_train,
+                     train_small=list_urls_train_small,
+                     test=list_urls_test)
 
     def __init__(self, root,
-                 scenes=['sun3d'],
                  num_frame_per_fragment=50,
-                 train=True,
+                 mode='train_small',
                  transform=None,
                  pre_transform=None,
                  pre_filter=None,
@@ -66,14 +85,11 @@ class Descr3DMatch(InMemoryDataset):
         Args:
             root (string): Root directory where the dataset should be saved
 
-            scenes (list, optional): list of string which indicate which
-                dataset we should download. (default: obj:`['sun3d']`)
-
             num_frame_per_fragment (int, optional): indicate the number of frames
                 we use to build fragments. If it is equal to 0, then we don't
                 build fragments and use the raw frames.
 
-            train (bool, optional): If :obj:`True`, loads the training dataset,
+            mode (string, optional): If :obj:`True`, loads the training dataset,
             otherwise the test dataset. (default: :obj:`True`)
 
             transform (callable, optional): A function/transform that takes in
@@ -95,49 +111,89 @@ class Descr3DMatch(InMemoryDataset):
         self.debug = debug
 
         self.num_frame_per_fragment = num_frame_per_fragment
-        self.scenes = scenes
-        super(Descr3DMatch, self).__init__(root,
-                                           transform,
-                                           pre_transform,
-                                           pre_filter)
+        self.mode = mode
+        if mode not in self.dict_urls.keys():
+            raise RuntimeError('this mode {} does '
+                               'not exist'
+                               '(train_small|train|test)'.format(mode))
+        super(General3DMatch, self).__init__(root,
+                                             transform,
+                                             pre_transform,
+                                             pre_filter)
 
-        path = self.processed_paths[0] if train else self.processed_paths[1]
-        self.data, self.slices = torch.load(path)
+        # path = self.processed_paths[0] if train else self.processed_paths[1]
+        # self.data, self.slices = torch.load(path)
+
+    @property
+    def fragment_dir(self):
+        return osp.join(self.root, 'fragment')
+
+    @property
+    def fragment_paths(self):
+        r"""The filepaths to find in the :obj:`self.processed_dir`
+        folder in order to skip the processing."""
+        files = to_list(self.fragment_file_names)
+        return [osp.join(self.fragment_dir, f) for f in files]
 
     @property
     def raw_file_names(self):
-        return self.scenes
+        return self.mode
 
     @property
     def processed_file_names(self):
-        pass
+        return self.mode
+
+    @property
+    def fragment_file_names(self):
+        return self.mode
 
     def download(self):
-        for scene in self.scenes:
-            if(self.verbose):
-                log.info("Download elements of the scene {}...".format(scene))
-            folder = osp.join(self.raw_dir, scene)
-            try:
-                for url in self.dict_urls[scene]:
-                    path = download_url(url, folder, self.verbose)
-                    extract_zip(path, folder, self.verbose)
-                    os.unlink(path)
-            except KeyError:
-                log.error("Could not download this file: {} because"
-                          "{} doesn't belong to"
-                          "3DMatch Dataset".format(scene, scene))
+        folder = osp.join(self.raw_dir, self.mode)
+        log.info("Download elements in the file {}...".format(folder))
+        for url in self.dict_urls[self.mode]:
+            path = download_url(url, folder, self.verbose)
+            extract_zip(path, folder, self.verbose)
+            os.unlink(path)
 
-    def create_fragment(self):
+    def _create_fragment(self):
         r"""
         create fragments from the rgbd frames ie a partial reconstruction of
         the scene with some frames(usually 50).
+        We will only use the first sequence for each scene
         """
+        if files_exist(self.fragment_paths):  # pragma: no cover
+            return
+        print("Create fragment from RGBD frames...")
+        makedirs(self.fragment_paths)
+        for scene_path in os.listdir(osp.join(self.raw_dir, self.mode)):
+            frames_dir = osp.join(self.raw_dir, self.mode,
+                                  scene_path, 'seq_01')
+            out_dir = osp.join(self.fragment_dir, self.mode, scene_path)
+            path_intrinsic = osp.join(self.raw_dir,
+                                      self.mode, scene_path,
+                                      'camera-intrinsics.txt')
+            list_path_frames = sorted([osp.join(frames_dir, f)
+                                       for f in osp.listdir(frames_dir)
+                                       if 'png' in f and 'depth' in f])
+            list_path_trans = sorted([osp.join(frames_dir, f)
+                                      for f in osp.listdir(frames_dir)
+                                      if 'pose' in f and 'txt' in f])
+            # compute each fragment and save it
+            rgbd2fragment(list_path_frames, path_intrinsic,
+                          list_path_trans, out_dir,
+                          self.num_frame_per_fragment)
 
-    def select_patch_from_fragment(self):
-        pass
 
-    def select_patch_from_frame(self):
-        pass
+
+
+    def compute_and_save(self):
+        raise NotImplementedError("Implement here the computation of the "
+                                  "training set"
+                                  "")
 
     def process(self):
+        self._create_fragment()
+        self.compute_and_save()
+
+    def get(self, idx):
         pass
--- src/datasets/registration/utils.py
+++ src/datasets/registration/utils.py
@@ -1,10 +1,24 @@
 import numpy as np
+import os.path as osp
 import torch
 import imageio
 
 
-def extract_pcd(depth, intrinsic):
-    return None
+def extract_pcd(depth_image, K):
+    """
+    transform a depth image into a pointcloud (here a numpy array)
+    """
+
+    Z = (depth_image/1000).ravel()
+    mask_z = (Z < 6) * (Z > 0)
+    X, Y = np.meshgrid(np.arange(depth_image.shape[1]), np.arange(depth_image.shape[0]))
+
+    Xworld = (X.ravel()[mask_z] + 0.5 - K[0, 2])*Z[mask_z]/K[0, 0]
+    Yworld = (Y.ravel()[mask_z] + 0.5 - K[1, 2])*Z[mask_z]/K[1, 1]
+
+    pcd = np.vstack((Xworld, Yworld, Z[mask_z])).T
+    return pcd
+
 
 
 def rgbd2pcd(path_img, path_intrinsic, path_trans):
@@ -20,16 +34,20 @@ def rgbd2pcd(path_img, path_intrinsic, path_trans):
 
 def rgbd2fragment(list_path_img,
                   path_intrinsic, list_path_trans,
+                  out_path,
                   num_frame_per_fragment=50):
 
-    list_fragment = []
     one_fragment = []
+    ind = 0
     for i, path_img in enumerate(list_path_img):
         path_trans = list_path_trans[i]
         pcd = rgbd2pcd(path_img, path_intrinsic, path_trans)
         one_fragment.append(pcd)
         if i + 1 % num_frame_per_fragment == 0:
-            list_fragment.append(np.concatenate(one_fragment, axis=0))
+            torch_pcd = torch.from_numpy(np.concatenate(one_fragment, axis=0))
+            torch.save(torch_pcd, osp.join(out_path,
+                                           'fragment_{}'.format(ind)))
+            ind += 1
             one_fragment = []
     # concatenate all fragment
 
