7b163c30dae712cf6df560d4874f392b7697f5c5
Nicolas Chaulet <nicolas.chaulet@gmail.com>
2020-01-22 17:15:14 +0000

    All runs

--- test/test_models.py
+++ test/test_models.py
@@ -25,61 +25,45 @@ def _find_model_using_name(model_class, task, model_config, dataset):
     return find_model_using_name(model_class, task, model_config, dataset)
 
 
-def _find_random_dataset(datasets):
-    dataset_names = datasets.keys()
-    idx = np.random.choice(len(dataset_names))
-    return getattr(datasets, list(dataset_names)[idx])
-
-
-def load_model_config(task, model_type, training):
+def load_model_config(task, model_type):
     models_conf = os.path.join(ROOT, "conf/models/{}/{}.yaml".format(task, model_type))
     config = OmegaConf.load(models_conf)
-    config = OmegaConf.merge(config, training)
-    return getattr(config.models, task)
+    return config.models
+
 
 class TestModelUtils(unittest.TestCase):
     def setUp(self):
-        self.cfg_training = OmegaConf.load(os.path.join(ROOT, "conf/training.yaml"))
         self.model_type_files = glob(os.path.join(ROOT, "conf/models/*/*.yaml"))
 
     def test_createall(self):
         for type_file in self.model_type_files:
-            
-            associated_task = type_file.split('/')[-2]
-            models_config = OmegaConf.load(type_file)
-            models_config = getattr(models_config.models, associated_task)
-            
-            dataset_paths = glob(os.path.join(ROOT, "conf/data/{}/*".format(associated_task)))
-            
-            for dataset_p in dataset_paths:
-                dataset_conf = getattr(OmegaConf.load(dataset_p).data, associated_task)
-                dataset_conf = getattr(dataset_conf, dataset_p.split('/')[-1].replace('.yaml', ''))
 
-                for model_name in models_config.keys():
-                    print(model_name)
-                    if model_name not in ["MyTemplateModel"]:
-                        model_config = models_config[model_name]
+            associated_task = type_file.split("/")[-2]
+            models_config = OmegaConf.load(type_file).models
+            for model_name in models_config.keys():
+                print(model_name)
+                if model_name not in ["MyTemplateModel"]:
+                    model_config = models_config[model_name]
+                    _find_model_using_name(
+                        model_config.architecture, associated_task, model_config, MockDatasetGeometric(6)
+                    )
 
-                        cfg_training = set_format(model_config, self.cfg_training)
-                        model_config = merges_in_sub(model_config, [cfg_training, dataset_conf])
-                        _find_model_using_name(model_config.architecture, associated_task, model_config, MockDatasetGeometric(6))
-    
     def test_pointnet2(self):
-        params = load_model_config("segmentation", "pointnet2", self.cfg_training)['pointnet2']
+        params = load_model_config("segmentation", "pointnet2")["pointnet2"]
         dataset = MockDatasetGeometric(5)
         model = _find_model_using_name(params.architecture, "segmentation", params, dataset)
         model.set_input(dataset[0])
         model.forward()
 
     def test_kpconv(self):
-        params = load_model_config("segmentation", "kpconv", self.cfg_training)['SimpleKPConv']
+        params = load_model_config("segmentation", "kpconv")["SimpleKPConv"]
         dataset = MockDatasetGeometric(5)
         model = _find_model_using_name(params.architecture, "segmentation", params, dataset)
         model.set_input(dataset[0])
         model.forward()
 
     def test_pointnet2ms(self):
-        params = load_model_config("segmentation", "pointnet2", self.cfg_training)['pointnet2ms']
+        params = load_model_config("segmentation", "pointnet2")["pointnet2ms"]
         dataset = MockDatasetGeometric(5)
         model = _find_model_using_name(params.architecture, "segmentation", params, dataset)
         model.set_input(dataset[0])
