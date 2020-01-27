6cc5a015ccd405f0f108d819a67aeeaa717466ea
chaton <thomas.chaton.ai@gmail.com>
2020-01-27 11:09:07 +0000

    add activation resolver (#134)
    * add activation resolver
    
    * typo

---
+++ src/utils/model_building_utils/activation_resolver.py
@@ -0,0 +1,18 @@
+import torch.nn
+
+from src.utils.config import is_dict
+
+def get_activation(act_opt, create_cls=True):
+    if is_dict(act_opt):
+        act_opt = dict(act_opt)
+        act = getattr(torch.nn, act_opt["name"])
+        del act_opt["name"]
+        args = dict(act_opt)
+    else:
+        act = getattr(torch.nn, act_opt)
+        args = {}
+
+    if create_cls:
+        return act(**args)
+    else:
+        return act
