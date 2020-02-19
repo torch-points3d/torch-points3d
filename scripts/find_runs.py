d0a69eafce1fb162cfe8a411fcae228ff52309df
Thomas Chaton <thomas.chaton.ai@gmail.com>
2020-02-19 10:55:49 +0000

    add stats as a property for pdb exploration

--- scripts/find_runs.py
+++ scripts/find_runs.py
@@ -21,6 +21,7 @@ class ExperimentFolder:
     def __init__(self, run_path):
         self._run_path = run_path
         self._model_name = None
+        self._stats = None
         self._find_files()
 
     def _find_files(self):
@@ -49,6 +50,7 @@ class ExperimentFolder:
     def extract_stats(self):
         path_to_checkpoint = os.path.join(self._run_path, self.model_name)
         stats = torch.load(path_to_checkpoint)["stats"]
+        self._stats = stats
         num_epoch = len(stats["train"])
         stats_dict = defaultdict(dict)
         for split_name in stats.keys():
@@ -58,6 +60,10 @@ class ExperimentFolder:
                     if "best" in metric_name:
                         stats_dict[metric_name][split_name] = latest_epoch[metric_name]
         return num_epoch, stats_dict
+
+    @property
+    def stats(self):
+        return self._stats
 
 
 def main(args):
