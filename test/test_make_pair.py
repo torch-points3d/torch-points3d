fd5392a7c57f1c5e8d4445122da0d316988ce2a0
humanpose1 <sofiane.horache@mines-paristech.fr>
2020-03-28 20:52:38 +0100

    correction pair test

--- test/test_make_pair.py
+++ test/test_make_pair.py
@@ -1,27 +1,26 @@
 import unittest
 import torch
-from torch_geometric.data import Data
-from src.datasets.registration.pair import make_pair
+from src.datasets.registration.pair import Pair
 
 
 class TestMakePair(unittest.TestCase):
     def simple_test(self):
         nb_points_1 = 101
-        data_source = Data(
+        data_source = Pair(
             pos=torch.randn((nb_points_1, 3)),
             x=torch.randn((nb_points_1, 9)),
             norm=torch.randn((nb_points_1, 3)),
             random_feat=torch.randn((nb_points_1, 15)),
         )
         nb_points_2 = 105
-        data_target = Data(
+        data_target = Pair(
             pos=torch.randn((nb_points_2, 3)),
             x=torch.randn((nb_points_2, 9)),
             norm=torch.randn((nb_points_2, 3)),
             random_feat=torch.randn((nb_points_2, 15)),
         )
 
-        b = make_pair(data_source, data_target)
+        b = Pair.make_pair(data_source, data_target)
         self.assertEqual(b.pos.size(), (nb_points_1 + nb_points_2, 3))
         self.assertEqual(b.x.size(), (nb_points_1 + nb_points_2, 9))
         print("pair:", b.pair)
