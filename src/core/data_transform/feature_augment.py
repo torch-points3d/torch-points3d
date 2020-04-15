3093bbe52587d6f96eb70c5dab5e946b51df6b3a
humanpose1 <sofiane.horache@mines-paristech.fr>
2020-04-15 23:02:09 +0200

    minor

--- src/core/data_transform/feature_augment.py
+++ src/core/data_transform/feature_augment.py
@@ -142,7 +142,7 @@ class Jitter:
 
     def __call__(self, data):
         if(random.random() < 0.95):
-            data.x = torch.randn_like(data.x) * self.sigma + mu
+            data.x = torch.randn_like(data.x) * self.sigma + self.mu
         return data
 
     def __repr__(self):
