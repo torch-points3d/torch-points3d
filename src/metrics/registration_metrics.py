623ceb9bd86f14baed838b7aa90ad678bb3b9ccb
humanpose1 <sofiane.horache@mines-paristech.fr>
2020-04-17 17:57:40 +0200

    tools to compute some interesting metrics

--- src/metrics/registration_metrics.py
+++ src/metrics/registration_metrics.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
@@ -23,25 +24,132 @@ def compute_accuracy(embedded_ref_features, embedded_val_features):
     return accuracy
 
 
-def compute_accuracy_gpu(embedded_ref_features, embedded_val_features):
-    pass
+def estimate_transfo(xyz, xyz_target):
+    """
+    estimate the rotation and translation using Kabsch algorithm
+    Parameters:
+    xyz :
+    xyz_target:
+    """
+    assert xyz.shape == xyz.shape
+    xyz_c = xyz - xyz.mean(0)
+    xyz_target_c = xyz_target - xyz_target.mean(0)
+    Q = xyz_c.T.mm(xyz_target_c) / len(xyz)
+    U, S, V = torch.svd(Q)
+    d = torch.det(V.mm(U.T))
+    R = V.mm(torch.diag(torch.tensor([1, 1, d]))).mm(U.T)
+    t = xyz_target.mean(0) - R @ xyz.mean(0)
+    T = torch.eye(4)
+    T[:3, :3] = R
+    T[:3, 3] = t
+    return T
 
 
-def estimate_transfo(xyz, xyz_target, match):
-    pass
+def get_geman_mclure_weight(xyz, xyz_target, mu):
+    """
+    compute the weights defined here for the iterative reweighted least square.
+    http://vladlen.info/papers/fast-global-registration.pdf
+    """
+    norm2 = torch.norm(xyz_target - xyz, dim=1) ** 2
+    return (mu / (mu + norm2)).view(-1, 1)
 
 
-def get_matches(feat, feat_target, num_matches=None):
-    pass
+def get_cross_product_matrix(k):
+    return torch.tensor([[0, -k[2], k[1]], [k[2], 0, -k[0]], [-k[1], k[0], 0]])
 
 
-def fast_global_registration(xyz, xyz_target, match):
-    pass
+def rodrigues(axis, theta):
+    """
+    given an axis of norm one and an angle, compute the rotation matrix using rodrigues formula
+    source : https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
+    """
+    K = get_cross_product_matrix(axis)
+    t = torch.tensor([theta])
+    R = torch.eye(3) + torch.sin(t) * K + (1 - torch.cos(t)) * K.mm(K)
+    return R
 
 
-def compute_hit_ratio(xyz, xyz_target, matches, T_gt, tau_1):
-    pass
+def get_matrix_system(xyz, xyz_target, weight):
+    """
+    Build matrix of size 3N x 6 and b of size 3N
+
+    xyz size N x 3
+    xyz_target size N x 3
+    weight size N
+    the matrix is minus cross product matrix concatenate with the identity (rearanged).
+    """
+    assert xyz.shape == xyz_target.shape
+    A_x = torch.zeros(xyz.shape[0], 6)
+    A_y = torch.zeros(xyz.shape[0], 6)
+    A_z = torch.zeros(xyz.shape[0], 6)
+    b_x = weight.view(-1) * (xyz_target[:, 0] - xyz[:, 0])
+    b_y = weight.view(-1) * (xyz_target[:, 1] - xyz[:, 1])
+    b_z = weight.view(-1) * (xyz_target[:, 2] - xyz[:, 2])
+    A_x[:, 1] = weight.view(-1) * xyz[:, 2]
+    A_x[:, 2] = -weight.view(-1) * xyz[:, 1]
+    A_x[:, 3] = weight.view(-1) * 1
+    A_y[:, 0] = -weight.view(-1) * xyz[:, 2]
+    A_y[:, 2] = weight.view(-1) * xyz[:, 0]
+    A_y[:, 4] = weight.view(-1) * 1
+    A_z[:, 0] = weight.view(-1) * xyz[:, 1]
+    A_z[:, 1] = -weight.view(-1) * xyz[:, 0]
+    A_z[:, 5] = weight.view(-1) * 1
+    return torch.cat([A_x, A_y, A_z], 0), torch.cat([b_x, b_y, b_z], 0).view(-1, 1)
+
+
+def get_trans(x):
+    """
+    get the matrix
+    """
+    T = torch.eye(4)
+    T[:3, 3] = x[3:]
+    axis = x[:3]
+    theta = torch.norm(axis)
+    if theta > 0:
+        axis = axis / theta
+    T[:3, :3] = rodrigues(axis, theta)
+    return T
+
+
+def fast_global_registration(xyz, xyz_target, mu_init=1, num_iter=20):
+    """
+    estimate the rotation and translation using Fast Global Registration algorithm (M estimator for robust estimation)
+    http://vladlen.info/papers/fast-global-registration.pdf
+    """
+    assert xyz.shape == xyz_target.shape
+
+    T_res = torch.eye(4)
+    mu = mu_init
+    source = xyz.clone()
+    weight = torch.ones(len(source), 1)
+    for i in range(num_iter):
+        if i > 0 and i % 5 == 0:
+            mu /= 2.0
+        A, b = get_matrix_system(source, xyz_target, weight)
+        x, _ = torch.solve(A.T @ b, A.T.mm(A))
+        T = get_trans(x.view(-1))
+        source = source.mm(T[:3, :3].T) + T[:3, 3]
+        T_res = T @ T_res
+        weight = get_geman_mclure_weight(source, xyz_target, mu)
+    return T_res
+
+
+def compute_hit_ratio(xyz, xyz_target, T_gt, tau_1):
+    """
+    compute proportion of point which are close.
+    """
+    assert xyz.shape == xyz.shape
+    dist = torch.norm(xyz.mm(T_gt[:3, :3].T) + T_gt[:3, 3] - xyz_target, dim=1)
+
+    return torch.mean((dist < 0.1).to(torch.float))
 
 
 def compute_transfo_error(T_gt, T_pred):
-    pass
+    """
+    compute the translation error (the unit depends on the unit of the point cloud)
+    and compute the rotation error in degree using the formula (norm of antisymetr):
+    http://jlyang.org/tpami16_go-icp_preprint.pdf
+    """
+    rte = torch.norm(T_gt[:3, 3] - T_pred[:3, 3])
+    rre = torch.acos((torch.trace(T_gt[:3, :3].mm(T_pred[:3, :3].T)) - 1) * 0.5) * 180 / np.pi
+    return rte, rre
--- test/test_registration_metrics.py
+++ test/test_registration_metrics.py
@@ -4,10 +4,10 @@ import unittest
 import numpy.testing as npt
 
 from src.metrics.registration_metrics import estimate_transfo
-from src.metrics.registration_metrics import get_matches
 from src.metrics.registration_metrics import fast_global_registration
 from src.metrics.registration_metrics import compute_hit_ratio
 from src.metrics.registration_metrics import compute_transfo_error
+from src.metrics.registration_metrics import rodrigues
 from src.core.data_transform import euler_angles_to_rotation_matrix
 
 
@@ -22,18 +22,9 @@ class TestRegistrationMetrics(unittest.TestCase):
         T_gt[:3, :3] = R_gt
         T_gt[:3, 3] = t_gt
         b = a.mm(R_gt.T) + t_gt
-        match = torch.stack([torch.arange(0, len(a)), torch.arange(0, len(a))]).T
-
-        T_pred = estimate_transfo(a, b, match)
+        T_pred = estimate_transfo(a, b)
 
         npt.assert_allclose(T_pred.numpy(), T_gt.numpy(), rtol=1e-3)
-
-    def test_get_matches(self):
-        feat = torch.tensor([[1, 0, 0], [0, 0, 1], [0.436, 0.9, 0]])
-        feat_target = torch.tensor([[-0.01, 0.01, 0.999], [0.42, 0.89, -0.12], [0.98, -0.17, 0.1]])
-        match_gt = torch.tensor([[0, 2], [1, 0], [2, 1]])
-        match_pred, _ = get_matches(feat, feat_target)
-        npt.assert_array_almost_equal(match_pred.numpy(), match_gt.numpy())
 
     def test_fast_global_registration(self):
         a = torch.randn(100, 3)
@@ -44,8 +35,20 @@ class TestRegistrationMetrics(unittest.TestCase):
         T_gt[:3, :3] = R_gt
         T_gt[:3, 3] = t_gt
         b = a.mm(R_gt.T) + t_gt
-        match = torch.stack([torch.arange(0, len(a)), torch.arange(0, len(a))]).T
-        T_pred = fast_global_registration(a, b, match)
+        T_pred = fast_global_registration(a, b, mu_init=1, num_iter=20)
+        npt.assert_allclose(T_pred.numpy(), T_gt.numpy(), rtol=1e-3)
+
+    def test_fast_global_registration_with_outliers(self):
+        a = torch.randn(100, 3)
+        R_gt = euler_angles_to_rotation_matrix(torch.rand(3) * np.pi)
+        t_gt = torch.rand(3)
+        T_gt = torch.eye(4)
+        T_gt[:3, :3] = R_gt
+        T_gt[:3, 3] = t_gt
+        b = a.mm(R_gt.T) + t_gt
+        b[[1, 5, 20, 32, 74, 17, 27, 77, 88, 89]] *= 42
+        T_pred = fast_global_registration(a, b, mu_init=1, num_iter=20)
+        # T_pred = estimate_transfo(a, b)
         npt.assert_allclose(T_pred.numpy(), T_gt.numpy(), rtol=1e-3)
 
     def test_compute_hit_ratio(self):
@@ -53,8 +56,22 @@ class TestRegistrationMetrics(unittest.TestCase):
         xyz_target = xyz.clone()
         xyz[[1, 5, 20, 32, 74, 17, 27, 77, 88, 89]] += 42
 
-        match = torch.stack([torch.arange(0, len(a)), torch.arange(0, len(a))]).T
+        hit = compute_hit_ratio(xyz, xyz_target, torch.eye(4), 0.1)
 
-        hit = compute_hit_ratio(xyz, xyz_target, match, torch.eye(4), 0.1)
+        self.assertAlmostEqual(hit.item(), 0.9)
 
-        self.assertAlmostEqual(hit, 0.9)
+    def test_compute_transfo_error(self):
+
+        axis = torch.randn(3)
+        axis = axis / torch.norm(axis)
+        theta = 30 * np.pi / 180
+
+        R = rodrigues(axis, theta)
+
+        T = torch.eye(4)
+        T[:3, :3] = R
+        T[0, 3] = 1
+
+        rte, rre = compute_transfo_error(torch.eye(4), T)
+        npt.assert_allclose(rte.item(), 1)
+        npt.assert_allclose(rre.item(), 30, rtol=1e-3)
