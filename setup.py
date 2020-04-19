2c696c7ceab9032b28dbf9be46ac28450afd3086
Thomas Chaton <thomas.chaton.ai@gmail.com>
2020-04-19 11:45:40 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ from setuptools import find_packages
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
-required = [r for r in required if "==" in r]
+install_requires = [r for r in required if "==" in r]
+install_requires[-1] = install_requires[-1].split(";")[0]
+dependency_links = [r[7:] for r in required if "-e git+" in r]
+install_requires += [l.split("=")[-1] for l in dependency_links]
 
 setup(
     name="torch-points3d",
@@ -14,7 +17,8 @@ setup(
     author="Thomas Chaton, Nicolas Chaulet",
     author_email="thomas.chaton.ai@gmail.com, nicolas.chaulet@gmail.com",
     license="MIT",
-    install_requires=required,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
     python_requires=">=3.6",
     packages=find_packages(),
     zip_safe=False,
