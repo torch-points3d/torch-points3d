b4a5259790c225ade13eec92c403782a78afeee6
humanpose1 <sofiane.horache@mines-paristech.fr>
2020-05-01 19:43:24 +0200

    adapt the tests

--- test/test_models.py
+++ test/test_models.py
@@ -13,7 +13,7 @@ from test.mockdatasets import MockDatasetGeometric, MockDataset
 from test.mockdatasets import PairMockDatasetGeometric, PairMockDataset
 
 from torch_points3d.models.model_factory import instantiate_model
-from torch_points3d.core.data_transform import ToSparseInput, XYZFeature
+from torch_points3d.core.data_transform import ToSparseInput, XYZFeature, GridSampling
 from torch_points3d.utils.model_building_utils.model_definition_resolver import resolve_model
 from torch_points3d.datasets.registration.pair import Pair, PairBatch, PairMultiScaleBatch, DensePairBatch
 from torch_geometric.transforms import Compose
@@ -64,14 +64,18 @@ class TestModelUtils(unittest.TestCase):
                 if conv_type.lower() == "dense":
                     return PairMockDataset(features, num_points=2048)
                 if conv_type.lower() == "sparse":
-                    tr = Compose([XYZFeature(True, True, True), ToSparseInput(0.01)])
+                    tr = Compose(
+                        [XYZFeature(True, True, True), GridSampling(size=0.01, quantize_coords=True, mode="last")]
+                    )
                     return PairMockDatasetGeometric(features, transform=tr, num_points=1024)
                 return PairMockDatasetGeometric(features)
             else:
                 if conv_type.lower() == "dense":
                     return MockDataset(features, num_points=2048)
                 if conv_type.lower() == "sparse":
-                    return MockDatasetGeometric(features, transform=ToSparseInput(0.01), num_points=1024)
+                    return MockDatasetGeometric(
+                        features, transform=GridSampling(size=0.01, quantize_coords=True, mode="last"), num_points=1024
+                    )
                 return MockDatasetGeometric(features)
 
         for type_file in self.model_type_files:
@@ -114,7 +118,7 @@ class TestModelUtils(unittest.TestCase):
 
     def test_siamese_minkowski(self):
         params = load_model_config("registration", "minkowski", "MinkUNet_Fragment")
-        transform = Compose([XYZFeature(True, True, True), ToSparseInput(grid_size=0.01)])
+        transform = Compose([XYZFeature(True, True, True), GridSampling(size=0.01, quantize_coords=True, mode="last")])
         dataset = PairMockDatasetGeometric(5, transform=transform, num_points=1024, is_pair_ind=True)
         model = instantiate_model(params, dataset)
         d = dataset[0]
