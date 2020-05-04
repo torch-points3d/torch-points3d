69f76db9cd8f3fc9ea906d5afb72429df1b8a4bb
Nicolas <nicolas.chaulet@gmail.com>
2020-05-04 09:02:16 +0100

    ME truly optional

--- torch_points3d/core/schedulers/bn_schedulers.py
+++ torch_points3d/core/schedulers/bn_schedulers.py
@@ -1,10 +1,20 @@
 from torch import nn
 import logging
-import MinkowskiEngine as ME
+
+try:
+    import MinkowskiEngine as ME
+except:
+    BATCH_NORM_MODULES = (
+        nn.BatchNorm1d,
+        nn.BatchNorm2d,
+        nn.BatchNorm3d,
+        ME.MinkowskiBatchNorm,
+        ME.MinkowskiInstanceNorm,
+    )
+else:
+    BATCH_NORM_MODULES = (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d)
 
 log = logging.getLogger(__name__)
-
-BATCH_NORM_MODULES = (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d, ME.MinkowskiBatchNorm, ME.MinkowskiInstanceNorm)
 
 
 def set_bn_momentum_default(bn_momentum):
