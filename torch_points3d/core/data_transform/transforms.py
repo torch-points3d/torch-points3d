7e5d5eb9a11b5983a7ef4080c91d619cac16a426
humanpose1 <47327126+humanpose1@users.noreply.github.com>
2020-06-29 09:32:57 +0200

    correct mistakes (#312)
    * correct mistakes
    
    * forget to change the tests
    
    * tired

--- torch_points3d/core/data_transform/transforms.py
+++ torch_points3d/core/data_transform/transforms.py
@@ -517,9 +517,11 @@ class ShiftVoxels:
 
     def __call__(self, data):
         if self._apply_shift:
-            if not isinstance(data.pos, torch.IntTensor):
+            if not hasattr(data, "coords"):
+                raise Exception("should quantize first using GridSampling3D")
+            if not isinstance(data.coords, torch.IntTensor):
                 raise Exception("The pos are expected to be coordinates, so torch.IntTensor")
-            data.pos[:, :3] += (torch.rand(3) * 100).type_as(data.pos)
+            data.coords[:, :3] += (torch.rand(3) * 100).type_as(data.coords)
         return data
 
     def __repr__(self):
