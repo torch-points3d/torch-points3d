05737102419c893a260bc843bf91f0850a12b905
Thomas Chaton <thomas.chaton.ai@gmail.com>
2020-07-14 20:54:13 +0100

    remove spaces

--- torch_points3d/utils/wandb_utils.py
+++ torch_points3d/utils/wandb_utils.py
@@ -4,24 +4,20 @@ import subprocess
 
 
 class WandbUrls:
-
     def __init__(self, url):
 
-        hash = url.split('/')[-2]
-        project = url.split('/')[-3]
-        entity = url.split('/')[-4]
+        hash = url.split("/")[-2]
+        project = url.split("/")[-3]
+        entity = url.split("/")[-4]
 
         self.weight_url = url
-        self.log_url = "https://app.wandb.ai/{}/{}/runs/{}/logs".format(
-            entity, project, hash)
-        self.chart_url = "https://app.wandb.ai/{}/{}/runs/{}".format(
-            entity, project, hash)
-        self.overview_url = "https://app.wandb.ai/{}/{}/runs/{}/overview".format(
-            entity, project, hash)
+        self.log_url = "https://app.wandb.ai/{}/{}/runs/{}/logs".format(entity, project, hash)
+        self.chart_url = "https://app.wandb.ai/{}/{}/runs/{}".format(entity, project, hash)
+        self.overview_url = "https://app.wandb.ai/{}/{}/runs/{}/overview".format(entity, project, hash)
         self.hydra_config_url = "https://app.wandb.ai/{}/{}/runs/{}/files/hydra-config.yaml".format(
-            entity, project, hash)
-        self.overrides_url = "https://app.wandb.ai/{}/{}/runs/{}/files/overrides.yaml".format(
-            entity, project, hash)
+            entity, project, hash
+        )
+        self.overrides_url = "https://app.wandb.ai/{}/{}/runs/{}/files/overrides.yaml".format(entity, project, hash)
 
     def __repr__(self):
         msg = "=================================================== WANDB URLS ===================================================================\n"
@@ -34,18 +30,18 @@ class WandbUrls:
 class Wandb:
     IS_ACTIVE = False
 
-    @ staticmethod
+    @staticmethod
     def set_urls_to_model(model, url):
         wandb_urls = WandbUrls(url)
         model.wandb = wandb_urls
 
-    @ staticmethod
+    @staticmethod
     def _set_to_wandb_args(wandb_args, cfg, name):
         var = getattr(cfg.wandb, name, None)
         if var:
             wandb_args[name] = var
 
-    @ staticmethod
+    @staticmethod
     def launch(cfg, launch: bool):
         if launch:
             import wandb
@@ -74,10 +70,8 @@ class Wandb:
             Wandb._set_to_wandb_args(wandb_args, cfg, "config")
 
             try:
-                commit_sha = subprocess.check_output(
-                    ["git", "rev-parse", "HEAD"]).decode("ascii").strip()
-                gitdiff = subprocess.check_output(
-                    ["git", "diff", "--", "':!notebooks'"]).decode()
+                commit_sha = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
+                gitdiff = subprocess.check_output(["git", "diff", "--", "':!notebooks'"]).decode()
             except:
                 commit_sha = "n/a"
                 gitdiff = ""
@@ -91,8 +85,7 @@ class Wandb:
 
             wandb.init(**wandb_args)
             shutil.copyfile(
-                os.path.join(os.getcwd(
-                ), ".hydra/config.yaml"), os.path.join(os.getcwd(), ".hydra/hydra-config.yaml")
+                os.path.join(os.getcwd(), ".hydra/config.yaml"), os.path.join(os.getcwd(), ".hydra/hydra-config.yaml")
             )
             wandb.save(os.path.join(os.getcwd(), ".hydra/hydra-config.yaml"))
             wandb.save(os.path.join(os.getcwd(), ".hydra/overrides.yaml"))
