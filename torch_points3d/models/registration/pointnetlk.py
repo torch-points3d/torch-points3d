039ca36a7540c32c7ea98265c720f1388302e094
humanpose1 <sofiane.horache@mines-paristech.fr>
2020-09-04 18:05:53 +0200

    add in a dirty way pointnet

--- torch_points3d/models/registration/pointnetlk.py
+++ torch_points3d/models/registration/pointnetlk.py
@@ -5,7 +5,7 @@ taken from https://github.com/hmgoforth/PointNetLK
 """
 import torch
 
-
+from torch_geometric.data import Data
 from torch_points3d.models.registration.base import End2EndBasedModel
 import torch_points3d.core.geometry.se3 as se3
 import torch_points3d.core.geometry.invmat as invmat
@@ -16,7 +16,108 @@ from torch_points3d.core.losses import MatrixMSELoss, RSQLoss
 from torch_points3d.core.geometry.se3 import SE3Transform
 
 
-class PointnetLK(End2EndBasedModel):
+class Pooling(torch.nn.Module):
+    def __init__(self, pool_type="max"):
+        self.pool_type = pool_type
+        super(Pooling, self).__init__()
+
+    def forward(self, input):
+        if self.pool_type == "max":
+            return torch.max(input, 2)[0].contiguous()
+        elif self.pool_type == "avg" or self.pool_type == "average":
+            return torch.mean(input, 2).contiguous()
+
+
+class PointNet(torch.nn.Module):
+    def __init__(self, emb_dims=1024, input_shape="bnc", use_bn=False, global_feat=True):
+        # emb_dims:			Embedding Dimensions for PointNet.
+        # input_shape:		Shape of Input Point Cloud (b: batch, n: no of points, c: channels)
+        super(PointNet, self).__init__()
+        if input_shape not in ["bcn", "bnc"]:
+            raise ValueError("Allowed shapes are 'bcn' (batch * channels * num_in_points), 'bnc' ")
+        self.input_shape = input_shape
+        self.emb_dims = emb_dims
+        self.use_bn = use_bn
+        self.global_feat = global_feat
+        self.pooling = Pooling("max")
+
+        self.layers = self.create_structure()
+
+    def create_structure(self):
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 64, 1)
+        self.conv3 = torch.nn.Conv1d(64, 64, 1)
+        self.conv4 = torch.nn.Conv1d(64, 128, 1)
+        self.conv5 = torch.nn.Conv1d(128, self.emb_dims, 1)
+        self.relu = torch.nn.ReLU()
+
+        if self.use_bn:
+            self.bn1 = torch.nn.BatchNorm1d(64)
+            self.bn2 = torch.nn.BatchNorm1d(64)
+            self.bn3 = torch.nn.BatchNorm1d(64)
+            self.bn4 = torch.nn.BatchNorm1d(128)
+            self.bn5 = torch.nn.BatchNorm1d(self.emb_dims)
+
+        if self.use_bn:
+            layers = [
+                self.conv1,
+                self.bn1,
+                self.relu,
+                self.conv2,
+                self.bn2,
+                self.relu,
+                self.conv3,
+                self.bn3,
+                self.relu,
+                self.conv4,
+                self.bn4,
+                self.relu,
+                self.conv5,
+                self.bn5,
+                self.relu,
+            ]
+        else:
+            layers = [
+                self.conv1,
+                self.relu,
+                self.conv2,
+                self.relu,
+                self.conv3,
+                self.relu,
+                self.conv4,
+                self.relu,
+                self.conv5,
+                self.relu,
+            ]
+        return layers
+
+    def forward(self, input_data):
+        # input_data: 		Point Cloud having shape input_shape.
+        # output:			PointNet features (Batch x emb_dims)
+        if self.input_shape == "bnc":
+            num_points = input_data.pos.shape[1]
+            input_p = input_data.pos.permute(0, 2, 1)
+        else:
+            num_points = input_data.pos.shape[2]
+            if input_p.shape[1] != 3:
+                raise RuntimeError("shape of x must be of [Batch x 3 x NumInPoints]")
+
+        output = input_p
+        for idx, layer in enumerate(self.layers):
+            output = layer(output)
+            if idx == 1 and not self.global_feat:
+                point_feature = output
+
+        if self.global_feat:
+            p = self.pooling(output)
+            return Data(x=p, pos=input_p)
+        else:
+            output = self.pooling(output)
+            output = output.view(-1, self.emb_dims, 1).repeat(1, 1, num_points)
+            return torch.cat([output, point_feature], 1)
+
+
+class LKBasedModel(End2EndBasedModel):
     """
     class of PointnetLK to estimate transformation using Pointnet.
     """
@@ -28,12 +129,13 @@ class PointnetLK(End2EndBasedModel):
         self.transform = SE3Transform(conv_type=option.conv_type, trans_x=option.trans_options.trans_x)
 
         backbone_option = option.backbone
-        backbone_cls = getattr(models, backbone_option.model_type)
+        getattr(models, backbone_option.model_type)
         # self.encoder = ?  # TODO define abstraction
         # in case we update x by position every time
-        self.encoder = backbone_cls(
-            architecture="encoder", input_nc=dataset.feature_dimension, config=backbone_option.config
-        )
+        self.encoder = PointNet()
+        # self.encoder = backbone_cls(
+        #     architecture="encoder", input_nc=dataset.feature_dimension, config=backbone_option.config
+        # )
         # losses
         self.loss_names = ["loss", "loss_T", "loss_r"]
         self.lambda_T = option.loss_options.lambda_T
