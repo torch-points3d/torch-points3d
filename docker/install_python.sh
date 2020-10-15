75c0bf66c3898595526e813f91eacf5a6dfb4441
Nicolas Chaulet <nicolas.chaulet@gmail.com>
2020-10-15 16:20:59 +0000

    Cleanup docker builds

---
+++ docker/install_python.sh
@@ -0,0 +1,24 @@
+if [[ "$#" -ne 1 ]]
+then
+    echo "Usage: ./install_python.sh gpu"
+    exit 1
+fi
+
+python3 -m pip install -U pip
+
+if [ $1 == "gpu" ]; then
+    echo "Install GPU"
+    pip3 install pylint autopep8 flake8 pre-commit black mypy  # Dev tools
+    pip3 install setuptools>=41.0.0 
+    pip3 install torch==1.6.0 torchvision==0.7.0 
+    pip3 install MinkowskiEngine==v0.4.3 --install-option="--force_cuda" --install-option="--cuda_home=/usr/local/cuda"
+    pip3 install pycuda 
+else
+    echo "Install CPU"
+    pip3 install pylint autopep8 flake8 pre-commit black mypy  # Dev tools
+    pip3 install setuptools>=41.0.0 
+    pip3 install torch==1.6.0+cpu torchvision==0.7.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+    pip3 install MinkowskiEngine
+fi
+
+rm -rf /root/.cache
