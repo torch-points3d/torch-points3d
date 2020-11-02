b60dc0e26204659e2f693187f828a1389b622360
Nicolas <nicolas.chaulet@gmail.com>
2020-11-02 11:21:51 +0000

    SparseConv Forward to ignore y when missing

--- torch_points3d/models/segmentation/sparseconv3d.py
+++ torch_points3d/models/segmentation/sparseconv3d.py
@@ -24,13 +24,17 @@ class APIModel(BaseModel):
 
     def set_input(self, data, device):
         self.input = data
-        self.labels = data.y.to(self.device)
+        if data.y is not None:
+            self.labels = data.y.to(self.device)
+        else:
+            self.labels = None
 
     def forward(self, *args, **kwargs):
         features = self.backbone(self.input).x
         logits = self.head(features)
         self.output = F.log_softmax(logits, dim=-1)
-        self.loss_seg = F.nll_loss(self.output, self.labels, ignore_index=IGNORE_LABEL)
+        if self.labels is not None:
+            self.loss_seg = F.nll_loss(self.output, self.labels, ignore_index=IGNORE_LABEL)
 
     def backward(self):
         self.loss_seg.backward()
