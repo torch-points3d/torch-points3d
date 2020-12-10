a14e76f701b534a866beb92486eadbcd28885989
Nicolas <nicolas.chaulet@gmail.com>
2020-12-10 12:46:30 +0000

    Add minkowski back fro VN2

--- torch_points3d/applications/models.py
+++ torch_points3d/applications/models.py
@@ -1,13 +1,18 @@
 from .kpconv import KPConv
 from .pointnet2 import PointNet2
 from .rsconv import RSConv
+import logging
+
+log = logging.getLogger(__name__)
 
 try:
     from .sparseconv3d import SparseConv3d
 except:
-    import logging
-
-    log = logging.getLogger(__name__)
     log.warning(
         "Sparse convolutions are not supported, please install one of the available backends, MinkowskiEngine or MIT SparseConv"
     )
+
+try:
+    from .minkowski import Minkowski
+except:
+    log.warning("MinkowskiEngine is not installed.")
