417b937815c39f52309fe6fe40f9f01ba8148bea
liubigli <leonardo.gigli@mines-paristech.fr>
2021-05-10 23:12:31 +0200

    update to hydra 1.1

--- train.py
+++ train.py
@@ -1,19 +1,20 @@
 import hydra
+from hydra.core.global_hydra import GlobalHydra
 from omegaconf import OmegaConf
 from torch_points3d.trainer import Trainer
 
 
-@hydra.main(config_path="conf/config.yaml")
+@hydra.main(config_path="conf", config_name="config")
 def main(cfg):
     OmegaConf.set_struct(cfg, False)  # This allows getattr and hasattr methods to function correctly
     if cfg.pretty_print:
-        print(cfg.pretty())
+        print(OmegaConf.to_yaml(cfg))
 
     trainer = Trainer(cfg)
     trainer.train()
-
-    # https://github.com/facebookresearch/hydra/issues/440
-    hydra._internal.hydra.GlobalHydra.get_state().clear()
+    #
+    # # https://github.com/facebookresearch/hydra/issues/440
+    GlobalHydra.get_state().clear()
     return 0
 
 
