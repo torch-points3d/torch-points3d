7c6f706e123f714d27997790149bc1d58b466d8a
humanpose1 <sofiane.horache@mines-paristech.fr>
2021-06-16 10:01:13 +0200

    correct on segmentation

--- torch_points3d/models/segmentation/ms_svconv3d.py
+++ torch_points3d/models/segmentation/ms_svconv3d.py
@@ -25,12 +25,12 @@ class MS_SparseConvModel(APIModel):
         self.unet = UnetMSparseConv3d(
             option_unet.backbone,
             input_nc=option_unet.input_nc,
-            pointnet_nn=option_unet.get('pointnet_nn'),
-            post_mlp_nn=option_unet.get('post_mlp_nn'),
-            pre_mlp_nn=option_unet.get('pre_mlp_nn'),
-            add_pos=option_unet.add_pos,
-            add_pre_x=option_unet.add_pre_x,
-            aggr=option_unet.aggr,
+            pointnet_nn=getattr(option_unet, "pointnet_nn", None),
+            post_mlp_nn=getattr(option_unet, "post_mlp_nn", [64, 64, 32]),
+            pre_mlp_nn=getattr(option_unet, "pre_mlp_nn", None),
+            add_pos=getattr(option_unet, "add_pos", False),
+            add_pre_x=getattr(option_unet, "add_pre_x", False),
+            aggr=getattr(option_unet, "aggr", None),
             backend=option.backend,
         )
         if option.mlp_cls is not None:
