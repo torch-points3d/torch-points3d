33dc3c346551108230728ff5e3139e1021fc145f
Leo Stanislas <leo.stanislas@gmail.com>
2022-06-19 16:57:32 -0400

    Fix bug in pointnet2.py

--- train.py
+++ train.py
@@ -5,7 +5,6 @@ from torch_points3d.trainer import Trainer
 
 @hydra.main(config_path="conf", config_name="config", version_base="1.2")
 def main(cfg):
-    # TODO[Leo]: Remove this, it is False by default
     OmegaConf.set_struct(cfg, False)
     trainer = Trainer(cfg)
     trainer.train()
